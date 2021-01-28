aebe86a76632f77cfaa48aa0c1b6922412e9a8f5
samdan <samdan@ijikod.com>
2021-01-28 08:30:19 +0000

    added network request type

---
+++ app/src/main/java/com/ijikod/connectivitycheckapp/Factory.kt
@@ -0,0 +1,14 @@
+package com.ijikod.connectivitycheckapp
+
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+
+class Factory {
+
+    fun wifiRequest() : NetworkRequest =
+        NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build()
+}
